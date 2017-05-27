455ff526538422c69fc57a8f45fc0378dde23723
Sami Karilainen <sami@Samis-Air.home>
2017-05-27 21:01:49 +0300

    added timer and countdown placeholders

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Countdown = () => {
+  return (
+    <div>
+      <h3>Countdown.jsx</h3>
+    </div>
+  )
+}
+
+module.exports = Countdown;
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,18 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () => {
+var Nav = React.createClass({
+  render: function () {
     return (
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
             <li className="menu-text">React Timer App</li>
             <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+              <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
             </li>
             <li>
-              <Link to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+              <Link to="/countdown" activeClassName="active-link">Countdown</Link>
             </li>
           </ul>
         </div>
@@ -22,6 +23,7 @@ var Nav = () => {
         </div>
       </div>
     );
-};
+  }
+});
 
 module.exports = Nav;
