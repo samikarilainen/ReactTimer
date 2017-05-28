6b112af6871017e99def15c068311fd5b6499b4e
Sami Karilainen <sami@Samis-Air.home>
2017-05-28 14:20:47 +0300

    Added controls and test

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ module.exports = {
       Nav: 'app/components/Nav.jsx',
       Timer: 'app/components/Timer.jsx',
       Countdown: 'app/components/Countdown.jsx',
+      Controls: 'app/components/Controls.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
       Clock: 'app/components/Clock.jsx',
       applicationStyles: 'app/styles/app.scss'
